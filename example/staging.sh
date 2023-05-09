feecebeb61932483b087f3c73dbd4ce4653b54a2
arafai <andrei.rafai@disciplemedia.com>
2023-05-09 18:40:44 +0100

    config setup for staging

---
+++ example/staging.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# export TERRAFORM_WORKSPACE=jason-local-farm-runner
+export TF_STATE_BUCKET="disciple-terraform-state-staging"
+export TF_STATE_OBJECT_KEY="eu-west-1"
+export TF_LOCK_DB="tf-lock-table"
+export AWS_REGION="eu-west-1"
+
+PRIVATE_SUBNETS='["subnet-039b7f0671156685f","subnet-0be179aba1c2c5c2e","subnet-0bd36c21b6fe54653"]'
+PUBLIC_SUBNETS='["subnet-04c11ff1dda8ec26b","subnet-00a33be38e5d8e2d6","subnet-0e0b250c708661ce9"]'
+
+export TF_VAR_route53_zone_id="Z00127123QLM4LWVF7RSG"
+export TF_VAR_route53_domain_name="staging.dscpl.net"
+export TF_VAR_vpc_id="vpc-037167ad1292a15ca"
+export TF_VAR_efs_subnet_ids=${PRIVATE_SUBNETS}
+export TF_VAR_jenkins_controller_subnet_ids=${PRIVATE_SUBNETS}
+export TF_VAR_alb_subnet_ids=${PUBLIC_SUBNETS}
+export TF_VAR_jenkins_dns_alias="jenkins"
--- example/variables.tf
+++ example/variables.tf
@@ -1,7 +1,6 @@
 variable route53_domain_name {
   type        = string
   description = "The domain"
-  default     = "staging.dscpl.net"
 }
 
 variable route53_zone_id {
@@ -10,7 +9,6 @@ variable route53_zone_id {
 The route53 zone id where DNS entries will be created. Should be the zone id
 for the domain in the var route53_domain_name.
 EOF
-  default     = "Z00127123QLM4LWVF7RSG"
 }
 
 variable jenkins_dns_alias {
@@ -19,29 +17,28 @@ variable jenkins_dns_alias {
 The DNS alias to be associated with the deployed jenkins instance. Alias will
 be created in the given route53 zone
 EOF
-  default     = "jenkins"
+  default     = "jenkins-controller"
 }
 
 variable vpc_id {
   type        = string
   description = "The vpc id for where jenkins will be deployed"
-  default     = "vpc-037167ad1292a15ca"
 }
 
 variable efs_subnet_ids {
   type        = list(string)
   description = "A list of subnets to attach to the EFS mountpoint. Should be private"
-  default = ["subnet-039b7f0671156685f","subnet-0be179aba1c2c5c2e","subnet-0bd36c21b6fe54653"]
+#   default = ["subnet-5d12c221","subnet-2178df6d","subnet-29452043"]
 }
 
 variable jenkins_controller_subnet_ids {
   type        = list(string)
   description = "A list of subnets for the jenkins controller fargate service. Should be private"
-  default = ["subnet-039b7f0671156685f","subnet-0be179aba1c2c5c2e","subnet-0bd36c21b6fe54653"]
+#   default = ["subnet-5d12c221","subnet-2178df6d","subnet-29452043"]
 }
 
 variable alb_subnet_ids {
   type        = list(string)
   description = "A list of subnets for the Application Load Balancer"
-  default = ["subnet-04c11ff1dda8ec26b","subnet-00a33be38e5d8e2d6","subnet-0e0b250c708661ce9"]
+#   default = ["subnet-5d12c221","subnet-2178df6d","subnet-29452043"]
 }
